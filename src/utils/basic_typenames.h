281ff273eb4a277dd1491b7d0742768cf0b96bc5
Edgar Ivan Vazquez Camargo <edgar21_9@hotmail.com>
2019-08-16 16:44:40 -0500

    added type names

---
+++ src/utils/basic_typenames.h
@@ -0,0 +1,6 @@
+#include <string.h>
+#define INT_NAME typeid(int).name()
+#define CHAR_NAME typeid(char).name()
+#define FLOAT_NAME typeid(float).name()
+#define LONG_NAME typeid(long).name()
+#define STRING_NAME typeid(std::string).name()
