d93ab1769eb7edc32cb6fa5f57d486a2bada84b0
Edgar Vazquez <edgar21_9@hotmail.com>
2019-08-20 11:53:49 -0500

    changed makefile aded rules

--- src/query/interpreter/interpreter.cpp
+++ src/query/interpreter/interpreter.cpp
@@ -4,8 +4,9 @@
 using namespace std;
 
 int main() {
-  YY_BUFFER_STATE buffer = yy_scan_string("MATCH");
-  yyparse();
+  YY_BUFFER_STATE buffer = yy_scan_string("ELEMENT");
+  int result = yyparse();
+  cout<<result<<endl;
   yy_delete_buffer(buffer);
-  return 0;
+  return result;
 }
