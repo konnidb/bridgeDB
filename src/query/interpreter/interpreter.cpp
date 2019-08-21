2362671cbeb6ff996d633aa4ffb9fd15de385b32
Edgar Vazquez <edgar21_9@hotmail.com>
2019-08-20 20:22:18 -0500

    changed ci conf

--- src/query/interpreter/interpreter.cpp
+++ src/query/interpreter/interpreter.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main() {
-  YY_BUFFER_STATE buffer = yy_scan_string("ELEMENT");
+  YY_BUFFER_STATE buffer = yy_scan_string("MATCH");
   int result = yyparse();
   cout<<result<<endl;
   yy_delete_buffer(buffer);
