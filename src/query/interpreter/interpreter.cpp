0a4bcb4da27e3b760a775610c12c785cb62be636
Edgar Vazquez <edgar21_9@hotmail.com>
2019-08-21 19:59:18 -0500

    format file

--- src/query/interpreter/interpreter.cpp
+++ src/query/interpreter/interpreter.cpp
@@ -2,10 +2,11 @@
 #include "lex.yy.c"
 #include "parser.tab.c"
 using namespace std;
-int main() {
-  YY_BUFFER_STATE buffer = yy_scan_string("MATCH (n:Person) RETURN n.property");
-  int result = yyparse();
-  cout<<result<<endl;
-  yy_delete_buffer(buffer);
-  return result;
+int main()
+{
+	YY_BUFFER_STATE buffer = yy_scan_string("MATCH (n:Person) RETURN n.property");
+	int result = yyparse();
+	cout << result << endl;
+	yy_delete_buffer(buffer);
+	return result;
 }
