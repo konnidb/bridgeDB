e81899206876e8f9109bba43dde9112055ee4ed4
Edgar Vazquez <edgar21_9@hotmail.com>
2019-09-02 11:06:26 -0500

    formatted code

--- src/query/interpreter/Interpreter.hpp
+++ src/query/interpreter/Interpreter.hpp
@@ -7,52 +7,52 @@ using namespace std;
 
 struct interpreter_node
 {
-  char *name;
-  char *schema_name;
-  unordered_map<string, void *> values;
+	char *name;
+	char *schema_name;
+	unordered_map<string, void *> values;
 };
 struct interpreter_edge
 {
-  char *name;
-  char *schema_name;
-  char *label;
+	char *name;
+	char *schema_name;
+	char *label;
 };
 
 class Interpreter
 {
 private:
-  int parser_result = 0;
-  bool is_inside_match;
-  string query;
-  unordered_map<string, void*> nodes;
-  unordered_map<string, void*> matched_nodes;
-  unordered_map<string, string> node_alias;
-  unordered_map<string, void*> edges;
-  unordered_map<string, void*> matcher_edges;
-  unordered_map<string, string> edge_alias;
+	int parser_result = 0;
+	bool is_inside_match;
+	string query;
+	unordered_map<string, void *> nodes;
+	unordered_map<string, void *> matched_nodes;
+	unordered_map<string, string> node_alias;
+	unordered_map<string, void *> edges;
+	unordered_map<string, void *> matcher_edges;
+	unordered_map<string, string> edge_alias;
 
 public:
-  Interpreter(string query);
-  void match(void);
-  void return_clause(void);
-  void create_node_id(string);
-  void create_node_id(char*);
-  void create_node_alias(string, string);
-  void assign_node_value(string, void*);
-  void create_edge_id(string);
-  void create_edge_alias(string, string);
-  void assign_edge_value(string, void*);
-  string get_query(void);
-  void set_parse_result(int);
-  int get_parse_result(void);
+	Interpreter(string query);
+	void match(void);
+	void return_clause(void);
+	void create_node_id(string);
+	void create_node_id(char *);
+	void create_node_alias(string, string);
+	void assign_node_value(string, void *);
+	void create_edge_id(string);
+	void create_edge_alias(string, string);
+	void assign_edge_value(string, void *);
+	string get_query(void);
+	void set_parse_result(int);
+	int get_parse_result(void);
 
-// Getters
-  unordered_map<string, void*> get_nodes_map(void);
-  void* get_node_value(string);
-  void* get_node_value_by_alias(string);
-  unordered_map<string, void*> get_edges_map(void);
-  void *get_edge_value(string);
-  void *get_edge_value_by_alias(string);
-  ~Interpreter();
+	// Getters
+	unordered_map<string, void *> get_nodes_map(void);
+	void *get_node_value(string);
+	void *get_node_value_by_alias(string);
+	unordered_map<string, void *> get_edges_map(void);
+	void *get_edge_value(string);
+	void *get_edge_value_by_alias(string);
+	~Interpreter();
 };
 #endif
