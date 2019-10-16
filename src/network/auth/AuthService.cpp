07f131d99e87e600b3f38f3062a58619e94313b0
Edgar Vazquez <edgar21_9@hotmail.com>
2019-10-16 17:57:37 -0500

    added submodule

--- src/network/auth/AuthService.cpp
+++ src/network/auth/AuthService.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include "AuthService.hpp"
+#include "jwt-cpp/jwt.h"
 using namespace std;
 
 AuthService::AuthService()
@@ -11,8 +12,13 @@ AuthService::~AuthService()
 {
 }
 
-string AuthService::generate_token()
+string AuthService::generate_token(string username, string database)
 {
+    auto token = jwt::create()
+                     .set_issuer("auth0")
+                     .set_type("JWS")
+                     .set_payload_claim(username, database)
+                     .sign(jwt::algorithm::hs256{"supersecret"});
     return "THIS_IS_A_TOKEN";
 }
 
@@ -23,5 +29,10 @@ bool AuthService::validate_credentials(string username, string password)
 
 bool AuthService::validate_token(string token)
 {
+    auto decoded = jwt::decode(token);
+    auto verify = jwt::verify()
+                      .allow_algorithm(jwt::algorithm::hs256{"supersecret"})
+                      .with_issuer("auth0");
+    verify.verify(decoded);
     return true;
 }
